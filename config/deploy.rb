e9f41f80e5874ed8fa962288dcee4017533356a2
Nacchamu <china.natsuki@gmail.com>
2017-05-04 11:43:03 +0900

    Revise mistake C

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,8 +9,14 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
 
-set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['.ssh/nacchamu_chat-space.pem']
+# set :ssh_options, auth_methods: ['publickey'],
+#                   keys: ['.ssh/nacchamu_chat-space.pem']
+
+set :ssh_options, {
+  # ローカル環境のユーザーの秘密鍵ファイルを設定する
+  keys: %w(/home/local_user_home/.ssh/chat-space_key_rsa),
+  forward_agent: true,
+}
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
