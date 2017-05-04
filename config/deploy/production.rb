9f4464b4aacceb565c3bfe2f9193ac840387dac8
Nacchamu <china.natsuki@gmail.com>
2017-05-04 11:33:42 +0900

    Revise mistake B

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,1 +1,6 @@
 server '52.196.66.172', user: 'natsuki-cs', roles: %w{app db web}
+set :ssh_options, {
+  # ローカル環境のユーザーの秘密鍵ファイルを設定する
+  keys: %w(/home/local_user_home/.ssh/chat-space_key_rsa),
+  forward_agent: true,
+}
